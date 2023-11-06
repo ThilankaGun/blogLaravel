184d7323aed526432e8ce50bb90c09bb558ccb21
ThilankaGun <thilankagunawardana.it@gmail.com>
2023-11-06 22:25:37 +0530

    turbo boost with modelfactories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,52 +15,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+//        User::truncate();
+//        Category::truncate();
+//        Post::truncate();
+        $user = User::factory()->create([
+           'name' => 'Windy',
+        ]);
+        Post::factory(5)->create([
+            'user_id' => $user->id,
+        ]);
+
         // \App\Models\User::factory(10)->create();
-        $user = \App\Models\User::factory()->create();
+//        $user = \App\Models\User::factory()->create();
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' =>$user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'excerpt' => '<p>Excerpt for my post</p>',
-            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
-            'slug' => 'my-family-post',
-        ]);
-
-        Post::create([
-            'user_id' =>$user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'excerpt' => '<p>Excerpt for my post</p>',
-            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
-            'slug' => 'my-work-post',
-        ]);
-
-        Post::create([
-            'user_id' =>$user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'excerpt' => '<p>Excerpt for my post</p>',
-            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
-            'slug' => 'my-personal-post',
-        ]);
+//        $personal = Category::create([
+//            'name' => 'Personal',
+//            'slug' => 'personal'
+//        ]);
+//
+//        $family = Category::create([
+//            'name' => 'Family',
+//            'slug' => 'family'
+//        ]);
+//
+//        $work = Category::create([
+//            'name' => 'Work',
+//            'slug' => 'work'
+//        ]);
+//
+//        Post::create([
+//            'user_id' =>$user->id,
+//            'category_id' => $family->id,
+//            'title' => 'My Family Post',
+//            'excerpt' => '<p>Excerpt for my post</p>',
+//            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
+//            'slug' => 'my-family-post',
+//        ]);
+//
+//        Post::create([
+//            'user_id' =>$user->id,
+//            'category_id' => $work->id,
+//            'title' => 'My Work Post',
+//            'excerpt' => '<p>Excerpt for my post</p>',
+//            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
+//            'slug' => 'my-work-post',
+//        ]);
+//
+//        Post::create([
+//            'user_id' =>$user->id,
+//            'category_id' => $personal->id,
+//            'title' => 'My Personal Post',
+//            'excerpt' => '<p>Excerpt for my post</p>',
+//            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
+//            'slug' => 'my-personal-post',
+//        ]);
     }
 }
