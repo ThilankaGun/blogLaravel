d30444a842043aafc864bd9ca4d9995cc5693ace
ThilankaGun <thilankagunawardana.it@gmail.com>
2023-11-06 21:32:03 +0530

    database seeding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Faker\Provider\en_UG\PhoneNumber;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use App\Models\User;
 
 class Post extends Model
 {
@@ -13,10 +15,15 @@ class Post extends Model
     //protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id'];
     protected $guarded = [];
 
-    public function category()
+    public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         //hasOne, hasMany, belongsTo, belongsToMany
         return $this->belongsTo(Category::class);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(\App\Models\User::class);
     }
 
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
         // \App\Models\User::factory(10)->create();
+        $user = \App\Models\User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' =>$user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'excerpt' => '<p>Excerpt for my post</p>',
+            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
+            'slug' => 'my-family-post',
+        ]);
+
+        Post::create([
+            'user_id' =>$user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'excerpt' => '<p>Excerpt for my post</p>',
+            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
+            'slug' => 'my-work-post',
+        ]);
+
+        Post::create([
+            'user_id' =>$user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Personal Post',
+            'excerpt' => '<p>Excerpt for my post</p>',
+            'body' => '<p>Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua. Uin ncididunt ut labore et dolore magna aliqua.</p>',
+            'slug' => 'my-personal-post',
+        ]);
     }
 }
