e5c48ebb4083f2a2c52ed18d83d90267ab317bd0
ThilankaGun <thilankagunawardana.it@gmail.com>
2023-11-06 18:01:33 +0530

    clockwork and n plus one problem

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    \Illuminate\Support\Facades\DB::listen(function ($query){
-        logger($query->sql, $query->bindings);
-    });
+//    \Illuminate\Support\Facades\DB::listen(function ($query){
+//        logger($query->sql, $query->bindings);
+//    });
 
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
