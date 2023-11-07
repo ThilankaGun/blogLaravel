25765da7dc360125eca3cf054d4de8b0fcb09468
ThilankaGun <thilankagunawardana.it@gmail.com>
2023-11-07 16:37:14 +0530

    convert the html and css

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,47 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <!--using "@" is a blade directives, instead using < ? php.... ?>-->
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}} ">
-                    {{$post->title}}
-                </a>
-            </h1>
-
-            <p>
+    @include('_posts-header')
 
 
-                By <a href="/authors/{{ $post->author->username }}">{{$post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
 
-            <div>
-                {{ $post->excerpt}}
-            </div>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
 
 
-        </article>
-    @endforeach
+
+        </div>
+    </main>
+{{--    @foreach ($posts as $post)--}}
+{{--        <!--using "@" is a blade directives, instead using < ? php.... ?>-->--}}
+{{--        <article>--}}
+{{--            <h1>--}}
+{{--                <a href="/posts/{{$post->slug}} ">--}}
+{{--                    {{$post->title}}--}}
+{{--                </a>--}}
+{{--            </h1>--}}
+
+{{--            <p>--}}
+
+
+{{--                By <a href="/authors/{{ $post->author->username }}">{{$post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+{{--            </p>--}}
+
+{{--            <div>--}}
+{{--                {{ $post->excerpt}}--}}
+{{--            </div>--}}
+
+
+{{--        </article>--}}
+{{--    @endforeach--}}
 
 </x-layout>
 
