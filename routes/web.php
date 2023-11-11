a1df8a03d8b9ef600a0626f02daef1bde1654374
ThilankaGun <thilankagunawardana.it@gmail.com>
2023-11-11 11:54:57 +0530

    search the messy way

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,17 @@ Route::get('/', function () {
     //        logger($query->sql, $query->bindings);
     //    });
 
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
+
     return view('posts', [
-        'posts' => Post::latest()->get(),
+        'posts' => $posts->get(),
+        //'posts' => Post::latest()->get(),
         'categories' => \App\Models\Category::all(),
     ]);
 })->name('home');
